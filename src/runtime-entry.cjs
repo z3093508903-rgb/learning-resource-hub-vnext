e754c6177deee3e9c5c8560d30de9cc533912215
时运Ⅲ <z3093508903@gmail.com>
2026-08-25 19:47:02 +0800

    feat(immersive): activate remembered note target and companion events

--- src/runtime-entry.cjs
+++ src/runtime-entry.cjs
@@ -2,10 +2,14 @@
 
 const ResourceHubNextPlugin = require('./entry.cjs');
 const { installScopedUiFixes } = require('./ui-fixes.cjs');
+const { registerRememberedNoteTarget } = require('./note-target.cjs');
+const { registerCompanionEventPoller } = require('./companion-events.cjs');
 
 class ResourceHubNextRuntimePlugin extends ResourceHubNextPlugin {
   async onload() {
     await super.onload();
+    registerRememberedNoteTarget(this);
+    registerCompanionEventPoller(this);
     installScopedUiFixes(this);
   }
 }
